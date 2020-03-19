8326266fa579be2c80381716855f195d64396c3e
nacho <nacho.conso@gmail.com>
2020-03-18 22:03:44 -0300

    nada

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ require("./routes/billingRoutes")(app);
 if (process.env.NODE_ENV === "production") {
   //Express will serve up production assets
   //like our main.js file, or main.css file!
-  app.use(express.static("/client/build"));
+  app.use(express.static("client/build"));
   //Express will serve up the index.html file
   //if it doesn't recognize the route
   const path = require("path");
