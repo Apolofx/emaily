c0cfc008f136036c83d09aed9660ea14378e72cc
nacho <nacho.conso@gmail.com>
2020-03-25 19:56:23 -0300

    added sendgrid integration

---
+++ services/Mailer.js
@@ -0,0 +1,34 @@
+const sendgrid = require("sendgrid");
+const helper = sendgrid.mail;
+const keys = require("../config/keys");
+
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email("no-replay@emaily.com");
+    this.subject = subject;
+    this.body = new helper.Content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const addClickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(ClickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+}
+
+module.exports = Mailer;
