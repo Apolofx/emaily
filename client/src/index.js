242080a1ef9164a3483fc896210a5489aa08fa24
nacho <nacho.conso@gmail.com>
2020-04-10 15:33:32 -0300

    added emails tests with axios

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,10 @@ import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";
 
+//dev only lines
+import axios from "axios";
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
